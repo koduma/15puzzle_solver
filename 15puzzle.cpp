c546b4d5c47675f01055929e862e83c478bc0b5a
tekitouk <47982907+koduma@users.noreply.github.com>
2025-05-24 13:28:00 +0900

    Update 15puzzle.cpp

--- 15puzzle.cpp
+++ 15puzzle.cpp
@@ -37,7 +37,7 @@ using namespace std;
 #define ROW 4
 #define COL 4
 #define TRN 200
-#define BW 45000
+#define BW 50000
 #define BW2 1
 
 typedef unsigned long long ll;
@@ -45,43 +45,53 @@ typedef unsigned long long ll;
 struct node {
 char score;
 ll hash;
-ll ans[(TRN/16)+1];
+ll ans[(TRN/21)+1];
+char prev;
 }fff[4*BW],ggg[4*BW2];
 
 string bestans;
 
 ll zoblish_field[ROW][COL][ROW*COL];
 
-char operation(char board[ROW][COL], ll movei[(TRN/16)+1]) {
-    char pos[ROW * COL] = {0};
-    for (char i = 0; i < ROW * COL; i++) {
-        pos[(int)board[i / COL][i % COL]] = i;
-    }
-    char zero_pos=pos[0];
-    for (char i = 0; i <= TRN / 16; i++) {
-        if (movei[i] == 0ll) {
-            break;
-        }
-        for (int j = 0; j < 16; j++) {
-            int dir = (int)(15ll & (movei[i] >> (4 * j)));
-            if (dir == 0) {
-                break;
-            }
+char operation(char board[ROW][COL], ll movei[(TRN/21)+1]) {   
+char pos[ROW * COL] = {0};
+char number[ROW * COL] = {0};
+for (char i = 0; i < ROW * COL; i++) {
+pos[(int)board[i / COL][i % COL]] = i;
+number[(int)i]=board[i/COL][i%COL];
+}
+char zero_pos=pos[0];
+for (char i = 0; i <= TRN / 21; i++) {    
+if (movei[i] == 0ll) {break;}
+for (int j = 0; j < 21; j++) {
+int m = (int)(7ll & (movei[i] >> (3 * j)));   
+if (m == 0) {
+break;
+}
 
-            int zero_row = ((int)zero_pos) / COL;
-            int zero_col = ((int)zero_pos) % COL;
-            int move_row = ((int)pos[dir]) / COL;
-            int move_col = ((int)pos[dir]) % COL;
+int dir=-1;
+
+if(m==1){dir=(int)zero_pos-1;}
+else if(m==2){dir=(int)zero_pos-COL;}
+else if(m==3){dir=(int)zero_pos+COL;}
+else if(m==4){dir=(int)zero_pos+1;}
+
+int zero_row = ((int)zero_pos) / COL;
+int zero_col = ((int)zero_pos) % COL;
+int move_row = dir / COL;
+int move_col = dir % COL;
             
-            swap(board[zero_row][zero_col], board[move_row][move_col]);
+swap(board[zero_row][zero_col], board[move_row][move_col]);
 
-            pos[0] = pos[dir];
-            pos[dir] = zero_row * COL + zero_col;
+pos[0] = (char)dir;
+pos[number[dir]] = zero_pos;
 
-            zero_pos = (char)(move_row * COL + move_col);
-        }
-    }
-    return zero_pos;
+swap(number[dir], number[zero_pos]);
+
+zero_pos = (char)dir;
+}
+}
+return zero_pos;
 }
 
 char MH_EV(char board[ROW][COL]){
@@ -118,14 +128,46 @@ hash ^= zoblish_field[row][col][(int)num];
 }
 return hash;
 }
-string getans(ll movei[(TRN/16)+1]){
+string getans(char board[ROW][COL],ll movei[(TRN/21)+1]){
 string ans="";
-for (char i = 0; i <= TRN/16; i++) {
-if (movei[i] == 0ll) { break; }
-for(char k=0;k<16;k++){
-int dir = (int)(15ll&(movei[i]>>(4*k)));
-if (dir==0){break;}
-ans+=to_string(dir)+"\n";
+char pos[ROW * COL] = {0};
+char number[ROW * COL] = {0};
+for (char i = 0; i < ROW * COL; i++) {
+pos[(int)board[i / COL][i % COL]] = i;
+number[(int)i]=board[i/COL][i%COL];
+}
+char zero_pos=pos[0];
+for (char i = 0; i <= TRN / 21; i++) {
+if (movei[i] == 0ll) {break;}
+for (int j = 0; j < 21; j++) {
+int m = (int)(7ll & (movei[i] >> (3 * j)));
+if (m == 0) {
+break;
+}
+
+int dir=-1;
+
+if(m==1){dir=(int)zero_pos-1;}
+else if(m==2){dir=(int)zero_pos-COL;}
+else if(m==3){dir=(int)zero_pos+COL;}
+else if(m==4){dir=(int)zero_pos+1;}
+
+int zero_row = ((int)zero_pos) / COL;
+int zero_col = ((int)zero_pos) % COL;
+int move_row = dir / COL;
+int move_col = dir % COL;
+
+ans+=to_string((int)board[move_row][move_col])+"\n";    
+            
+swap(board[zero_row][zero_col], board[move_row][move_col]);
+
+pos[0] = (char)dir;
+pos[number[dir]] = zero_pos;
+
+swap(number[dir], number[zero_pos]);
+
+zero_pos = (char)dir;
+    
 }
 }
 return ans;
@@ -135,9 +177,10 @@ int BEAM_SEARCH(char board[ROW][COL]) {
 vector<node>dque;
 node n0;
 n0.hash=calc_hash(board);
-for(int i=0;i<=(TRN/16);i++){
+for(int i=0;i<=(TRN/21);i++){
 n0.ans[i]=0ll;
 }
+n0.prev=-1;    
 dque.push_back(n0);
 
 int dx[4] = { -1, 0,0,1 };
@@ -158,17 +201,17 @@ node cand = temp;
 int xxx=((int)zero_pos)%COL;
 int yyy=((int)zero_pos)/COL;
 if (0 <= xxx + dx[j] && xxx + dx[j] < COL &&
-0 <= yyy + dy[j] && yyy + dy[j] < ROW) {
+0 <= yyy + dy[j] && yyy + dy[j] < ROW && cand.prev+j!=3) {
 char board2[ROW][COL];
 memcpy(board2,temp_board,sizeof(board2));
 int ny=yyy + dy[j];
 int nx=xxx + dx[j];
-ll z=(ll)board2[ny][nx];
 cand.hash^=(zoblish_field[yyy][xxx][(int)board2[yyy][xxx]])^(zoblish_field[ny][nx][(int)board2[ny][nx]]);
 cand.hash^=(zoblish_field[yyy][xxx][(int)board2[ny][nx]])^(zoblish_field[ny][nx][(int)board2[yyy][xxx]]);
-cand.ans[i/16] |= z<<((4*i)%64);
+cand.ans[i/21] |= (((ll)(j+1))<<((3*i)%63));
 swap(board2[ny][nx],board2[yyy][xxx]);
 cand.score=MH_EV(board2);
+cand.prev=j;
 fff[(4 * k) + j] = cand;
 }
 else{
@@ -176,22 +219,30 @@ cand.score=125;
 fff[(4 * k) + j] = cand;
 }
 }
-}
+}   
 dque.clear();
+deque<int>dq[150];
 vector<pair<int,int> >vv;
 for (int j = 0; j < 4 * ks; j++) {
 if (fff[j].score == 0) {
-bestans=getans(fff[j].ans);
+bestans=getans(board,fff[j].ans);
 return i+1;
 }
-vv.push_back(make_pair(fff[j].score,j));
+//vv.push_back(make_pair(fff[j].score,j));
+dq[fff[j].score].push_front(j);
 }
-sort(vv.begin(),vv.end());
+//sort(vv.begin(),vv.end());
 int push_node=0;
-for (int j = 0; push_node < BW ;j++) {
-if(j>=(int)vv.size()){break;}
-int p=vv[j].second;
+char possible_score=0;
+for (int j = 0; push_node < BW ;j++) {    
+if(possible_score>=125){break;}
+if((int)dq[(int)possible_score].size()==0){
+possible_score++;
+continue;
+}
+int p=dq[(int)possible_score][0];
 node n1=fff[p];
+dq[(int)possible_score].pop_front();
 if (i < TRN - 1) {
 if(!visited[n1.hash]){
 visited[n1.hash]=true;
@@ -199,7 +250,7 @@ dque.push_back(n1);
 push_node++;
 }
 }
-}
+}  
 }
 return -1;
 }
@@ -208,9 +259,10 @@ int BEAM_SEARCH2(char board[ROW][COL]) {
 vector<node>dque;
 node n0;
 n0.hash=calc_hash(board);
-for(int i=0;i<=(TRN/16);i++){
+for(int i=0;i<=(TRN/21);i++){
 n0.ans[i]=0ll;
 }
+n0.prev=-1;    
 dque.push_back(n0);
 int dx[4] = { -1, 0,0,1 };
 int dy[4] = { 0,-1,1,0 };
@@ -228,24 +280,19 @@ node cand = temp;
 int xxx=((int)zero_pos)%COL;
 int yyy=((int)zero_pos)/COL;
 if (0 <= xxx + dx[j] && xxx + dx[j] < COL &&
-0 <= yyy + dy[j] && yyy + dy[j] < ROW) {
+0 <= yyy + dy[j] && yyy + dy[j] < ROW && cand.prev+j!=3) {
 char board2[ROW][COL];
 memcpy(board2,temp_board,sizeof(board2));
 int ny=yyy + dy[j];
 int nx=xxx + dx[j];
-ll z=(ll)board2[ny][nx];
 cand.hash^=(zoblish_field[yyy][xxx][(int)board2[yyy][xxx]])^(zoblish_field[ny][nx][(int)board2[ny][nx]]);
 cand.hash^=(zoblish_field[yyy][xxx][(int)board2[ny][nx]])^(zoblish_field[ny][nx][(int)board2[yyy][xxx]]);
-cand.ans[i/16] |= z<<((4*i)%64);
+cand.ans[i/21] |= (((ll)(j+1))<<((3*i)%63));
 swap(board2[ny][nx],board2[yyy][xxx]);
 cand.score=(char)BEAM_SEARCH(board2);
 if(cand.score==-1){cand.score=125;}
-if(cand.score<10){
-if(MH_EV(board2)==0){
-bestans=getans(cand.ans);
-return i+1;
-}    
-}
+if(cand.score<10){if(MH_EV(board2)==0){bestans=getans(board,cand.ans);return i+1;}}
+cand.prev=j;    
 ggg[(4 * k) + j] = cand;
 }
 else{
@@ -255,16 +302,22 @@ ggg[(4 * k) + j] = cand;
 }
 }
 dque.clear();
+deque<int>dq[150];
 vector<pair<int,int> >vv;
 for (int j = 0; j < 4 * ks; j++) {
-vv.push_back(make_pair(ggg[j].score,j));
+dq[ggg[j].score].push_front(j);
 }
-sort(vv.begin(),vv.end());
 int push_node=0;
-for (int j = 0; push_node < BW2 ;j++) {
-if(j>=(int)vv.size()){break;}
-int p=vv[j].second;
+char possible_score=0;
+for (int j = 0; push_node < BW2 ;j++) {  
+if(possible_score>=125){break;}
+if((int)dq[(int)possible_score].size()==0){
+possible_score++;
+continue;
+}
+int p=dq[(int)possible_score][0];
 node n1=ggg[p];
+dq[(int)possible_score].pop_front();
 if (i < TRN - 1) {
 if(!visited[n1.hash]){
 visited[n1.hash]=true;
